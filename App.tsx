446b4c43433dec33a96230e5a04dc52401397cf2
Borges10002 <borges10002@gmail.com>
2023-09-21 20:23:39 -0400

    finicher class organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.eventName}>Nome do evento </Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022. </Text>
     </View>
   );
 }
@@ -13,8 +14,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
   },
 });
